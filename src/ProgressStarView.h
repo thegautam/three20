3dd0a179c6686ddf9f8b12b0c42061549ab47fbf
Gautam Kedia <gautamkedia@gmail.com>
2010-05-16 12:29:41 -0700

    Star progress bar. (cherry picked from commit 2bb063b49031128a0efea5d6d982603bd47641da)

---
+++ src/ProgressStarView.h
@@ -0,0 +1,24 @@
+//
+//  ProgressStarView.h
+//  Happiness
+//
+//  Created by Gautam Kedia on 5/16/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@protocol ProgressStarViewDelegate
+@property (readonly) int progressCount;
+@property (readonly) int totalCount;
+@end
+
+
+@interface ProgressStarView : UIView {
+  id<ProgressStarViewDelegate> delegate;
+}
+
+@property (assign) id<ProgressStarViewDelegate> delegate; 
+
+@end
+

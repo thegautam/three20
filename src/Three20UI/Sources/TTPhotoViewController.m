d91738e56c5df675c98d2d8471e5b66dd96b4e22
Gautam Kedia <gautamkedia@gmail.com>
2012-08-28 07:29:41 -0700

    Whitespace change.

--- src/Three20UI/Sources/TTPhotoViewController.m
+++ src/Three20UI/Sources/TTPhotoViewController.m
@@ -93,7 +93,7 @@ static const NSInteger kActivityLabelTag          = 96;
     self.navigationBarStyle = UIBarStyleBlackTranslucent;
     self.navigationBarTintColor = nil;
     self.wantsFullScreenLayout = YES;
-    self.hidesBottomBarWhenPushed = YES;
+    self.hidesBottomBarWhenPushed = YES;;
 
     self.defaultImage = TTIMAGE(@"bundle://Three20.bundle/images/photoDefault.png");
   }
